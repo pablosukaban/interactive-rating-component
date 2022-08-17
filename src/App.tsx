69e6995a21b88bd837e8372a1fefdfda9731818f
ilya <zapaska96@gmail.com>
2022-08-17 10:13:19 +0500

    разметка

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="star">
+        <img src="./images/icon-star.svg" />
+      </div>
+      <h1>How did we do?</h1>
+      <h3>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </h3>
+      <div>1 2 3 4 5</div>
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default App;
