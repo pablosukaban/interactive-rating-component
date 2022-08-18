17877290ccbc0ea47e69dd94abaf818f5ddb5c82
ilya <zapaska96@gmail.com>
2022-08-18 22:27:03 +0500

    смена цвета при выделении

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -14,7 +14,6 @@ export const Rating = ({ setIsSubmitted, setChosenMark }: RatingProps) => {
 
   const tempMarks = useMemo(() => createMarks(), [])
   const [stateMarks, setStateMarks] = useState<MarkType[]>(tempMarks)
-  const [count, setCount] = useState(0)
 
   function createMarks() {
     let res = []
@@ -24,18 +23,25 @@ export const Rating = ({ setIsSubmitted, setChosenMark }: RatingProps) => {
         isPressed: false
       })
     }
-    console.log("marks created")
     return res;
   }
 
+  function handleClick(value: number) {
+    const newMarks = stateMarks.map(mark => {
+      if (mark.value !== value) return { ...mark, isPressed: false }
+      return { ...mark, isPressed: !mark.isPressed }
+    })
+    setStateMarks(newMarks)
+  }
+
+  console.log(stateMarks)
 
   return (
     <div className="rating-container">
       <div className="star">
         <img src="./images/icon-star.svg" />
       </div>
-      <h1 className="rating-title">How did we do? {count}</h1>
-      <button onClick={() => setCount(prev => (prev + 1))}>Click</button>
+      <h1 className="rating-title">How did we do?</h1>
       <h3 className="rating-para">
         Please let us know how we did with your support request. All feedback is
         appreciated to help us improve our offering!
@@ -44,6 +50,8 @@ export const Rating = ({ setIsSubmitted, setChosenMark }: RatingProps) => {
         {stateMarks.map(mark => (
           <li
             key={mark.value}
+            onClick={() => handleClick(mark.value)}
+            className={mark.isPressed ? "submitted-active" : ""}
           >
             {mark.value}
           </li>
